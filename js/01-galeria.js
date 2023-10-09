a2c80ae88b90827724fb4eb8484e08aa42ace5c1
JavierV23G <127901386+JavierV23G@users.noreply.github.com>
2023-10-08 19:56:06 -0500

    Last changes

---
+++ js/01-galeria.js
@@ -0,0 +1,50 @@
+import { galeriaItems } from './galeria-items.js';
+// Change code below this line
+
+const galeria = document.querySelector(".galeria");
+let instance;
+
+const createListItem = image => {
+  return `<li class="galeria__item">
+  <a class="galeria__link" href="${image.original}">
+    <img
+      class="galeria__image"
+      src="${image.preview}"
+      data-source="${image.original}"
+      alt="${image.description}"
+    />
+  </a>
+</li>`;
+};
+
+for (const image of galeriaItems) {
+  const newImage = createListItem(image);
+  galeria.innerHTML += newImage;
+}
+
+galeria.addEventListener("click", e => {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+  // console.log(e.target.dataset["source"]);
+  const imageInModal = e.target.dataset["source"];
+  openModal(imageInModal);
+});
+
+const openModal = imageSource => {
+  instance = basicLightbox.create(`<img width="1280" src="${imageSource}">`);
+  instance.show();
+
+  window.addEventListener("keydown", closeModal);
+};
+
+const closeModal = e => {
+  if (e.code === "Escape") {
+    window.removeEventListener("keydown", closeModal);
+
+    instance.close();
+  }
+};
+
+console.log(galeriaItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
+import { galeriaItems } from './galeria-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galeria = document.querySelector(".galeria");
+
+const createListItem = image => {
+  return `<li class="galeria__item">
+    <a class="galeria__link" href="${image.original}">
+        <img class="galeria__image" src="${image.preview}" alt="${image.description}" />
+    </a>
+</li>`;
+};
+
+for (const image of galeriaItems) {
+  const newImage = createListItem(image);
+  galeria.innerHTML += newImage;
+}
+
+var lightbox = new SimpleLightbox(".galeria a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  captionPosition: "bottom",
+});
+
+lightbox.on("show.simplelightbox", function (e) {
+  e.preventDefault();
+});
